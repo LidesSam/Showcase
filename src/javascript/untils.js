d92435363eeef1f46aa6c557f68db985a3721942
LidesSam <lides.sam@gmail.com>
2021-03-02 18:36:55 -0300

    v2.0

--- src/javascript/untils.js
+++ src/javascript/untils.js
@@ -1,68 +1,71 @@
-function _calculateAge(birthday) { // birthday is a date
+function calculateAge(birthday) { // birthday is a date
     var ageDifMs = Date.now() - birthday.getTime();
+
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    var age=Math.abs(ageDate.getUTCFullYear() - 1970);
-    document.getElementById("age").textContent = age.toString();
-    
+
+    var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    document.getElementById("age").textContent = age;
+
 
 }
-function addInversionCalculator(){
+
+function addInversionCalculator() {
     var container = document.getElementById("invCalc");
-    container.textContent="ssss";
-    container.style.color="white";
+    container.textContent = "ssss";
+    container.style.color = "white";
 
 
 
     var money = document.createElement("INPUT");
-    money.setAttribute("type","text");
-    money.setAttribute("id","money");
-    money.style.display="flexbox";
-    money.style.width="inherit";
+    money.setAttribute("type", "text");
+    money.setAttribute("id", "money");
+    money.style.display = "flexbox";
+    money.style.width = "inherit";
 
     container.appendChild(money);
     var days = document.createElement("INPUT");
-    days.setAttribute("type","text");
-    days.setAttribute("id","days");
-    days.style.display="flexbox";
-    days.style.width="inherit";
-    days.style.background.color="grey";
+    days.setAttribute("type", "text");
+    days.setAttribute("id", "days");
+    days.style.display = "flexbox";
+    days.style.width = "inherit";
+    days.style.background.color = "grey";
 
     container.appendChild(days);
 
     var days = document.createElement("INPUT");
-    days.setAttribute("type","text");
-    days.setAttribute("id","days");
-    days.style.display="flexbox";
-    days.style.width="inherit";
-    days.style.background.color="grey";
+    days.setAttribute("type", "text");
+    days.setAttribute("id", "days");
+    days.style.display = "flexbox";
+    days.style.width = "inherit";
+    days.style.background.color = "grey";
 
     container.appendChild(days);
 
 
     var calcbtn = document.createElement("button");
-    calcbtn.textContent="calculate";
-    calcbtn.style.display="flexbox";
-    calcbtn.style.width="inherit";
-    calcbtn.onclick=calculateProfit;
+    calcbtn.textContent = "calculate";
+    calcbtn.style.display = "flexbox";
+    calcbtn.style.width = "inherit";
+    calcbtn.onclick = calculateProfit;
     container.appendChild(calcbtn);
 
 
 
-    var result =document.createElement("div");
-    result.setAttribute("id","result");
-    result.textContent="R;";
-    result.style.color="white";
-    result.style.background="black";
+    var result = document.createElement("div");
+    result.setAttribute("id", "result");
+    result.textContent = "R;";
+    result.style.color = "white";
+    result.style.background = "black";
 
     container.appendChild(result);
 }
-
-function calculateProfit(){
+/**
+ * function calculateProfit() {
     alert("calc");
     var container = document.getElementById("invCalc");
     var money = parseint(container.getElementById("money").textContent);
-    alert("money:"+string(money));
-    var result =container.getElementById("result");
-    result.textContent=money;
+    alert("money:" + string(money));
+    var result = container.getElementById("result");
+    result.textContent = money;
 }
-
+ */
